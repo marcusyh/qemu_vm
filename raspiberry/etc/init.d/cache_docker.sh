b59f05cd1783cf650b361987ff7169f4dc5ae9c7
Gikyou Sha <sha-g@smflc.co.jp>
2021-07-18 10:32:34 +0000

    change to sysv init based, fixed some bugs

--- raspiberry/etc/init.d/cache_docker.sh
+++ raspiberry/etc/init.d/cache_docker.sh
@@ -1,99 +1,155 @@
 #!/bin/bash
 
+### BEGIN INIT INFO
+# Provides: cached_docker
+# Required-Start: $all
+# Required-Stop: $remote_fs $syslog
+# Required-Stop: 
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 6
+# Short-Description: put docker's root dir to a cache fs
+# Description:
+### END INIT INFO
+
 BASE_DIR=/docker
 CACHE_DIR=/cache/docker
+ROOTFS_PARTITION=/dev/rootfs
+ROOTFS_PARTITION_ALIAS=/dev/mmcblk0p2
+DEBUG=0 #0 print debug info. other, ignore debug
 
-function update_config(){
+# Get lsb functions
+. /lib/lsb/init-functions
+
+# if $ROOTFS_PARTITION if mounted as rw, return 0 (true), else return 1 (false)
+function rootfs_is_rw(){
+    grep "[[:space:]]rw[[:space:],]" /proc/mounts  |egrep "($ROOTFS_PARTITION)|($ROOTFS_PARTITION_ALIAS)" >/dev/null
+    rslt=$?
+    echo $rslt
+    return $rslt
+}
+ROOTFS_MNT_RW=$(rootfs_is_rw)
+
+function remount_rootfs_rw(){
+    MNT_TYPE=$1
+
+    if [ "$MNT_TYPE" = 1 ]
+    then
+        mount -o remount,ro $ROOTFS_PARTITION /
+    else
+        mount -o remount,rw,noatime,nodiratime $ROOTFS_PARTITION /
+    fi
+}
+
+function restore_rootfs_mount_type(){
+    current_mnt_rw=$(rootfs_is_rw)
+    echo $ROOTFS_MNT_RW $current_mnt_rw
+    if [ "$cuurent_mnt_rw" != "$ROOTFS_MNT_RW" ]
+    then
+        remount_rootfs_rw $ROOTFS_MNT_RW
+    fi
+}
+
+# In bash,
+# 0 means true
+# not 0 means false
+function config_need_update(){
     ROOT_DIR=$1
 
-    if [ $(systemctl is-active docker) = "active" ]
-    then
-        systemctl stop docker
-    fi
-
     # check is /etc/docker/daemon.json need to update
-    same_flag=0
     if [ -f /etc/docker/daemon.json ]
     then
         rslt=$(cat /etc/docker/daemon.json | grep "data-root" |sed "s/.*data-root.*: *\"\(.*\)\".*/\1/g")
         if [ $rslt = $ROOT_DIR ]
         then
-    	    same_flag=1
+            return 1
         fi
     fi
-    
+
+    return 0
+}
+
+function update_config(){
+    ROOT_DIR=$1
+
     # update docker root dir if /etc/docker/daemon.json is not exists
-    if [ $same_flag -eq 0 ]
+    rootfs_is_rw || remount_rootfs_rw 0
+    if [ ! -f /etc/docker/daemon.json ]
     then 
-        mount -o remount,rw,noatime,nodiratime /dev/sda2 /
     
-        if [ ! -f /etc/docker/daemon.json ]
-        then
-            cat <<EOT > /etc/docker/daemon.json
+        cat <<EOT > /etc/docker/daemon.json
 { 
    "data-root": "$ROOT_DIR" 
 }
 EOT
-        else
-            sed -i "s,\"data-root\": *.*,\"data-root\": \"$ROOT_DIR\",g"  /etc/docker/daemon.json
-        fi
-        mount -o remount,ro /dev/sda2 /
+    else
+        sed -i "s,\"data-root\": *.*,\"data-root\": \"$ROOT_DIR\",g"  /etc/docker/daemon.json
     fi
 }
 
-
-function get_image_base_layer(){
-    if [ $(systemctl is-active docker) != "active" ]
+# In bash,
+# 0 means true
+# not 0 means false
+function docker_is_live(){
+    docker images >/dev/null 2>&1
+    if [ $? -ne 0 ]
     then
-        systemctl start docker
+        return 1
     fi
 
-    image_layers_id=""
-    for image_id in $(docker images --format '{{.ID}}' |xargs)
-    do
-	for layer_id in $(docker inspect $image_id |grep LowerDir |sed 's#\s*"LowerDir": "\(.*\)",#\1#g' |sed 's#/diff# #g' |sed 's#:##g')
-	do
-	    layer_id=$(echo $layer_id |awk -F '/' '{print $(NF)}')
-	    image_layers_id="$image_layers_id $layer_id"
-	done
-    done
-    echo $image_layers_id
+    if [ -f /var/run/docker.pid ]
+    then
+        return 0
+    else
+        return 1
+    fi
 }
 
+function operate_docker(){
+    OPERATION=$1
 
-function cleanup_container(){
-    if [ $(systemctl is-active docker) != "active" ]
+    if [ $DEBUG -eq 0 ]
     then
-        systemctl start docker
+        /etc/init.d/docker $OPERATION
+    else
+        /etc/init.d/docker $OPERATION >/dev/null 2>&1
     fi
 
-    for container_id in $(docker ps --format '{{.ID}}')
+}
+
+function purge_cached_docker(){
+    docker_is_live || operate_docker start 
+
+    test $DEBUG -eq 0 && echo "cleaning up the the existing containers ..."
+    for container_id in $(docker ps --format '{{.ID}}' 2>/dev/null)
     do
-	docker stop $container_id
-	docker rm $container_id
+	docker stop $container_id >/dev/null 2&>1
+	docker rm $container_id >/dev/null 2&>1
     done
     
-    for container_id in $(docker ps -a --format '{{.ID}}')
+    for container_id in $(docker ps -a --format '{{.ID}}' 2>/dev/null)
     do
-	docker rm $container_id
+	docker rm $container_id >/dev/null 2&>1
     done
+
+    # stop docker service
+    docker_is_live && operate_docker stop
+
+    # make sure a clean cache dir is exists
+    test $DEBUG -eq 0 && echo "deleting root_data_dir of the cached varsion docker ..."
+    rm -rf $CACHE_DIR/* >/dev/null 2&>1
 }
 
 
-function deploy_cache_version(){
-    if [ $(systemctl is-active docker) = "active" ]
-    then
-        systemctl stop docker
-    fi
+function deploy_cached_docker(){
+    docker_is_live && operate_docker stop
 
     # make sure a clean cache dir is exists
     mkdir -p $CACHE_DIR && rm -rf $CACHE_DIR/*
 
-    cleanup_container
-    
     image_layers_id=$(cat $BASE_DIR/base_layers_list.txt)
     
     # copy all files except for the images layers
+    test $DEBUG -eq 0 && echo "deploying the new version cache from /docker"
     for layer_id in $(echo $image_layers_id)
     do
         PARAMS="$PARAMS --exclude=overlay2/$layer_id"
@@ -111,10 +167,7 @@ function deploy_cache_version(){
     # A sleep is also needed before start. 
     # or, there will be a error: The unit docker.service has entered the 'failed' state with result 'start-limit-hit
     # looks like the firewll or the fucking systemd was set a limit in somewhere.
-    systemctl start docker
-    sleep 3
-    systemctl stop docker
-    systemctl start docker
+    operate_docker start
 }
 
 
@@ -122,79 +175,113 @@ function check_baselayers_list(){
     # get images' layers list
     if [ ! -f $BASE_DIR/base_layers_list.txt ]
     then
-	echo $BASE_DIR/base_layers_list.txt is not found.
+	test $DEBUG -eq 0 && echo $BASE_DIR/base_layers_list.txt is not found, please run "/etc/init.d/cache_docker.sh deploy" first.
 	exit 1
     fi
 }
 
+function analysis_image_baselayers(){
+    docker_is_live || operate_docker start
 
-function set_baselayers_list(){
-    mount -o remount,rw,noatime,nodiratime /dev/sda2 /
+    image_layers_id=""
+    for image_id in $(docker images --format '{{.ID}}' |xargs)
+    do
+	for layer_id in $(docker inspect $image_id |grep LowerDir |sed 's#\s*"LowerDir": "\(.*\)",#\1#g' |sed 's#/diff# #g' |sed 's#:##g')
+	do
+	    layer_id=$(echo $layer_id |awk -F '/' '{print $(NF)}')
+	    image_layers_id="$image_layers_id $layer_id"
+	done
+    done
+    echo $image_layers_id
+}
 
-    if [ $(systemctl is-active docker) != "active" ]
-    then
-        systemctl start docker
-    fi
-    new_base_layers=$(get_image_base_layer)
+function update_baselayers_list(){
+    new_base_layers=$(analysis_image_baselayers)
+
     if [ -f $BASE_DIR/base_layers_list.txt ]
     then
         old_base_layers=$(cat $BASE_DIR/base_layers_list.txt |xargs)
         if [ "$new_base_layers" = "$old_base_layers" ]
         then
-	    return
+            return
         fi
     fi
-    echo $new_base_layers > $BASE_DIR/base_layers_list.txt
 
-    mount -o remount,ro /dev/sda2 /
+    rootfs_is_rw || remount_rootfs_rw 0
+    echo $new_base_layers > $BASE_DIR/base_layers_list.txt
 }
 
 
-
-
 case "$1" in
-
-    start|"")
+    start)
+	log_begin_msg "Starting cached version docker"
 	check_baselayers_list
-	update_config $CACHE_DIR
-	deploy_cache_version
-	exit $?
+	deploy_cached_docker
+
+	log_end_msg $?
 	;;
 
-    restart|"")
-	check_baselayers_list
-	deploy_cache_version
-	exit $?
-	;;
+    restart)
+	log_begin_msg "Restarting cached version docker"
 
-    reload)
-	update_config $BASE_DIR
-	set_baselayers_list
-	update_config $CACHE_DIR
-	deploy_cache_version
-	exit $?
+	check_baselayers_list
+
+        purge_cached_docker
+	deploy_cached_docker
+
+	log_end_msg $?
 	;;
 
     stop)
-    	if [ $(systemctl is-active docker) = "active" ]
-    	then
-    	    systemctl stop docker
-    	fi
-	rm -rf $CACHE_DIR/*
-	exit $?
+	log_begin_msg "Stopping cached version docker"
+
+        purge_cached_docker
+
+	log_end_msg $?
+	;;
+
+    deploy)
+	log_begin_msg "Deploy the cached version docker"
+
+	config_need_update $BASE_DIR && update_config $BASE_DIR
+        update_baselayers_list
+
+	config_need_update $CACHE_DIR && update_config $CACHE_DIR
+        restore_rootfs_mount_type
+
+        purge_cached_docker
+	deploy_cached_docker
+
+	log_end_msg $?
+	;;
+
+    active|enable)
+	log_begin_msg "Active the deployed cached version docker"
+
+	check_baselayers_list
+
+	config_need_update $CACHE_DIR && update_config $CACHE_DIR
+        restore_rootfs_mount_type
+
+        purge_cached_docker
+	deploy_cached_docker
+
+	log_end_msg $?
 	;;
 
     restore)
-	update_config $BASE_DIR
-	rm -rf $CACHE_DIR/*
-        mount -o remount,rw,noatime,nodiratime /dev/sda2 /
-	systemctl start docker
+	log_begin_msg "Restore docker to it's original setting"
 
-	exit $?
+        purge_cached_docker
+	config_need_update $BASE_DIR && update_config $BASE_DIR
+
+        rootfs_is_rw || remount_rootfs_rw 0
+	operate_docker start
+
+	log_end_msg $?
 	;;
 
     *)
-	echo f
 	exit 3
 	;;
 
--- raspiberry/etc/init.d/tmpfs_overlay.sh
+++ raspiberry/etc/init.d/tmpfs_overlay.sh
@@ -1,48 +1,111 @@
-#!/bin/bash
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides: var opt
+# Required-Start: mountkernfs udev eudev
+# Required-Stop: umountfs
+# Default-Start: S
+# Default-Stop: 0 6
+# Short-Description:  mount /var and /opt to overlayfs
+# Description:
+### END INIT INFO
+
+# update-rc.d tmpfs_overlay defaults S
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+. /lib/init/vars.sh
+. /lib/init/tmpfs.sh
+
+. /lib/lsb/init-functions
+. /lib/init/mount-functions.sh
+
+
+# domount function: Mount kernel and device file systems.
+# $1: mount mode (mount, remount)
+# $2: file system type
+# $3: alternative file system type (or empty string if none)
+# $4: mount point
+# $5: mount device name
+# $6... : extra mount program options
+
+
+do_start(){
+    # create /cache and mount tmpfs on it
+    log_daemon_msg "Mounting /cache"
+    domount mount tmpfs "" /cache tmpfs "-omode=1777,nosuid,nodev "
+    
+    # mount disk partitiion3 to /writable
+    log_daemon_msg "Mounting /writable"
+    domount mount ext4 "" /writable /dev/mmcblk0p3 "-onoatime,nodiratime"
+    
+    # create overlay mount dir
+    log_daemon_msg "Creating dirs for /var and /opt"
+    mkdir -p /cache/var /cache/opt /cache/overlay/var /cache/overlay/opt
+    mkdir -p /writable/var /writable/opt
+    
+    # mount /var
+    log_daemon_msg "mounting /var"
+    mount -t overlay overlay -o lowerdir=/writable/var:/var_ro,upperdir=/cache/var,workdir=/cache/overlay/var /var
+    
+    # mount /opt
+    log_daemon_msg "mounting /opt"
+    mount -t overlay overlay -o lowerdir=/writable/opt,upperdir=/cache/opt,workdir=/cache/overlay/opt /opt
+    
+    #log_daemon_msg "change / to ro mode"
+    #mount -o remount,ro,defaults /dev/sda2 /
+    
+    exit $?
+}
+
+do_stop(){
+    log_daemon_msg "umount /opt"
+    umount /opt
+    
+    log_daemon_msg "umount /var"
+    umount /var
+    
+    log_daemon_msg "save /cache/var to /writable/var"
+    rsync -ar /cache/var/ /writable/var
+    
+    log_daemon_msg "save /cache/opt to /writable/opt"
+    rsync -ar /cache/opt/ /writable/opt
+    
+    log_daemon_msg "umount /cache"
+    umount /cache
+    
+    log_daemon_msg "umount /writable"
+    umount /writable
+    
+    exit $?
+}
+
 
 case "$1" in
 
     start|"")
-	# create /cache and mount tmpfs on it
-	mkdir -p /cache
-	mount -t tmpfs /cache
-
-	# mount disk partitiion3 to /writable
-	mount -t ext4 -o noatime,nodiratime /dev/mmcblk0p3 /writable/
-
-	# create overlay mount dir
-	mkdir -p /cache/var /cache/opt /cache/overlay/var /cache/overlay/opt
-	mkdir -p /writable/var /writable/opt
-
-	# mount /var
-	mount -o remount,ro,defaults /dev/sda2 /
-	mount -t overlay overlay -o lowerdir=/writable/var:/var_bak,upperdir=/cache/var,workdir=/cache/overlay/var /var
-
-	# mount /opt
-	mount -t overlay overlay -o lowerdir=/writable/opt,upperdir=/cache/opt,workdir=/cache/overlay/opt /opt
-
-	systemctl daemon-reload
-	systemctl start systemd-timesyncd
-
-	exit $?
+	do_start
 	;;
 
-    restart|reload|force-reload)
-	exit 3
+    restart)
+	do_stop
+	do_start
 	;;
 
     stop)
-	umount /opt
-	umount /var
-	rsync -avr /cache/var/ /writable/var
-	umount /cache
-	umount /writable
-
-	exit $?
+	do_stop
 	;;
 
+    status)
+	df -h --output | egrep '(/var$)|(/opt$)|(/cache$)|(/writable$)|(Filesystem\s*Type\s*Inodes\s*IUsed\s*IFree)'
+	;;
+
+    reload|force-reload)
+	echo "Only start, stop, restart, status supported at current time"
+	exit 1
+	;;
     *)
-	exit 3
+        echo "Usage: ${0:-} {start|stop|restart|status}" >&2
+        exit 1
 	;;
 
 esac
